6aa094be243bc7f84b1b2946f77d77083be24c51
Tanbal <tanbal.derakhty@gmail.com>
2025-10-28 18:54:15 +0330

    Add Bootstrap to project
    skill-section-responsive

--- src/components/skill-card/Skill_Card.js
+++ src/components/skill-card/Skill_Card.js
@@ -5,10 +5,12 @@ class Skill_Card extends Component {
     render() {
         const {subject, score, image} = this.props;
         return (
-            <div id={this.props.id} className="skill-card">
-                <div className="skill-subject">{subject}</div>
-                <img src={image} />
-                <Progress_Bar score={score} />
+            <div className='col skill-card-area'>
+                <div id={this.props.id} className="skill-card">
+                    <div className="skill-subject">{subject}</div>
+                    <img src={image} />
+                    <Progress_Bar score={score} />
+                </div>
             </div>
         );
     }
